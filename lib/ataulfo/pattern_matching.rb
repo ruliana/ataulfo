2ee162ea1df3b73bfa7b0fd5ae44f875301d78f0
Ronie Uliana <ronie.uliana@gmail.com>
2012-10-19 00:18:23 -0300

    Refactoring: cleaning code
    Removing unused parameters
    Moving logic from "method_missing" to a better place
    Renaming "vars" to better match its role on diffent parts

--- lib/ataulfo/pattern_matching.rb
+++ lib/ataulfo/pattern_matching.rb
@@ -5,23 +5,21 @@ module Ataulfo
 
     class Body
       def initialize(vars, other_self)
-        @vars, @other_self = vars, other_self
+        @other_self = other_self
+        vars.each do |k, v|
+          define_singleton_method(k) { v.value }
+        end
       end
 
       def method_missing(method, *args, &block)
-        result = @vars[method]
-        if result
-          result.value
-        else
-          @other_self.send method, *args, &block
-        end
+        @other_self.send method, *args, &block
       end
     end
 
     class Matcher
       def initialize(object, pattern)
         @object                      = object
-        @vars                        = pattern.select { |_, v| v.is_a? Var }
+        @pattern_vars                = pattern.select { |_, v| v.is_a? Var }
         inner_matches                = pattern.select { |_, v| v.is_a? Hash }
         fixed_values                 = pattern.select { |_, v| !v.is_a?(Var) && !v.is_a?(Hash) }
         @object_respond_to_methods   = pattern.keys.all? { |k| @object.respond_to? k }
@@ -31,23 +29,19 @@ module Ataulfo
       end
 
       def matches?
-        match = false
-        if @object_respond_to_methods && @object_answers_right_values
-          match = @matchers.all?(&:matches?)
-        end
-        match
+        @matchers.all?(&:matches?) if @object_respond_to_methods && @object_answers_right_values
       end
 
       def fill_vars
-        @vars.each { |k, v| v.value = @object.send k }
+        @pattern_vars.each { |k, v| v.value = @object.send k }
         @matchers.each(&:fill_vars)
       end
     end
 
-    def initialize(object, other_self, vars = { })
-      @object     = object
-      @other_self = other_self
-      @vars       = vars
+    def initialize(object, other_self)
+      @object       = object
+      @other_self   = other_self
+      @context_vars = { }
     end
 
     def like(pattern, &block)
@@ -56,15 +50,15 @@ module Ataulfo
 
       matcher.fill_vars
 
-      Body.new(@vars, @other_self).instance_eval(&block)
-      @vars = { }
+      Body.new(@context_vars, @other_self).instance_eval(&block)
+      @context_vars = { }
     end
 
     def method_missing(method_name, *_)
       # The line below also returns the "Var" to the
       # pattern. That makes it a collector parameter
       # already distributed where it belongs to.
-      @vars[method_name] = Var.new
+      @context_vars[method_name] = Var.new
     end
   end
 end
