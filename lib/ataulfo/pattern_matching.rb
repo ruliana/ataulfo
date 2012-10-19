1e2af0df3651169f130718e6d7dada07e308ecc8
Ronie Uliana <ronie.uliana@gmail.com>
2012-10-18 23:56:43 -0300

    Refactoring: avoid a single method do 2 different things
    Problem was the old method was doing too much.
    
    I created a new class to handle what the previous method
    did and splitted the logic in two inside it.
    
    The end result was better than I expected, but there
    is an indirection on "vars" that's frightening.

--- lib/ataulfo/pattern_matching.rb
+++ lib/ataulfo/pattern_matching.rb
@@ -18,39 +18,52 @@ module Ataulfo
       end
     end
 
+    class Matcher
+      def initialize(object, pattern)
+        @object                      = object
+        @vars                        = pattern.select { |_, v| v.is_a? Var }
+        inner_matches                = pattern.select { |_, v| v.is_a? Hash }
+        fixed_values                 = pattern.select { |_, v| !v.is_a?(Var) && !v.is_a?(Hash) }
+        @object_respond_to_methods   = pattern.keys.all? { |k| @object.respond_to? k }
+        @object_answers_right_values = fixed_values.all? { |k, v| @object.send(k) == v }
+
+        @matchers = inner_matches.map { |k, v| Matcher.new(object.send(k), v) }
+      end
+
+      def matches?
+        match = false
+        if @object_respond_to_methods && @object_answers_right_values
+          match = @matchers.all?(&:matches?)
+        end
+        match
+      end
+
+      def fill_vars
+        @vars.each { |k, v| v.value = @object.send k }
+        @matchers.each(&:fill_vars)
+      end
+    end
+
     def initialize(object, other_self, vars = { })
       @object     = object
       @other_self = other_self
       @vars       = vars
     end
 
-    def fill_vars(pattern)
-      vars                        = -> { pattern.select { |_, v| v.is_a? Var } }
-      inner_matches               = -> { pattern.select { |_, v| v.is_a? Hash } }
-      fixed_values                = -> { pattern.select { |_, v| !v.is_a?(Var) && !v.is_a?(Hash) } }
-      object_respond_to_methods   = -> { pattern.keys.all? { |k| @object.respond_to? k } }
-      object_answers_right_values = -> { fixed_values[].all? { |k, v| @object.send(k) == v } }
-
-      match = false
-      if object_respond_to_methods[] && object_answers_right_values[]
-        match = true
-        vars[].each do |k, v|
-          v.value = @object.send k
-        end
-        inner_matches[].each do |k, v|
-          match &&= PatternMatching.new(@object.send(k), @other_self, @vars).fill_vars(v)
-        end
-      end
-      match
-    end
-
     def like(pattern, &block)
-      match = fill_vars(pattern)
-      Body.new(@vars, @other_self).instance_eval(&block) if match
+      matcher = Matcher.new(@object, pattern)
+      return unless matcher.matches?
+
+      matcher.fill_vars
+
+      Body.new(@vars, @other_self).instance_eval(&block)
       @vars = { }
     end
 
     def method_missing(method_name, *_)
+      # The line below also returns the "Var" to the
+      # pattern. That makes it a collector parameter
+      # already distributed where it belongs to.
       @vars[method_name] = Var.new
     end
   end
